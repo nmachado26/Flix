e8e5267a08d42524e36512eba622e5f37ebce051
Nicolas Machado <nmachado@fb.com>
2018-06-27 15:50:03 -0700

    basic template

---
+++ Flix/ViewControllers/MoviesViewController.m
@@ -0,0 +1,81 @@
+//
+//  MoviesViewController.m
+//  Flix
+//
+//  Created by Nicolas Machado on 6/27/18.
+//  Copyright © 2018 Nicolas Machado. All rights reserved.
+//
+
+#import "MoviesViewController.h"
+
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate> //class implements these two protocols, which is just a promise that I will implement its required methods. Command click to see required methods. Datasource is what it takes to show tableview content, delegate says hey ik how to respond to tableview events (scroll, select, etc)
+
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation MoviesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSLog(@"%@", dataDictionary);
+            self.movies = dataDictionary[@"results"];
+            for(NSDictionary *movie in self.movies){
+                NSLog(@"%@", movie[@"title"]);
+            }
+            
+            [self.tableView reloadData];
+            // TODO: Get the array of movies
+            // TODO: Store the movies in a property to use elsewhere
+            // TODO: Reload your table view data
+        }
+    }];
+    [task resume];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//if this function is not called, then you forgot to call the datasource = self call in viewdidload
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
+    
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    cell.textLabel.text = movie[@"title"];
+    //NSLog(@"%@", [NSString stringWithFormat:@"row: %d, section %d", indexPath.row, indexPath.section]);
+    return cell;
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
